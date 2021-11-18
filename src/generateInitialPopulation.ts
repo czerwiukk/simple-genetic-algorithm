518d576b7e1a34c0121ad9d9b5fc52471ecf84ce
czerwiukk <czerwiukk.dev@gmail.com>
2021-11-18 19:10:16 +0100

    use round

--- src/generateInitialPopulation.ts
+++ src/generateInitialPopulation.ts
@@ -1,5 +1,5 @@
 export const generateInitialPopulation = (individualsCount: number) => {
-  const generateByte = () => (Math.random() > 0.5 ? 1 : 0);
+  const generateByte = () => Math.round(Math.random());
 
   const generateIndividual = () =>
     Array.from(Array(8).keys()).map(generateByte).join("");
